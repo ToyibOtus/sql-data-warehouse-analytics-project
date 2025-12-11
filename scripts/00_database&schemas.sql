8251172e40035b3f9ab4668d1334d4e4bed9ce98
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-11 16:37:12 +0000

    Update and rename 00_init_database.sql to 00_database&schemas.sql

---
+++ scripts/00_database&schemas.sql
@@ -0,0 +1,50 @@
+/*
+==================================================================================================
+Create Database & Schemas
+==================================================================================================
+Script Purpose:
+	Creates the database [MyDatabase] and the Medallion Architecture schemas:
+    landing, bronze, silver, gold, audit.
+
+Warning:
+	This script permanently deletes the database [Mydatabase] and all data in it.
+	Ensure to have proper backup before running.
+==================================================================================================
+*/
+USE master;
+GO
+
+-- Check existence of [MyDatabase] and Drop it if exist
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'MyDatabase')
+BEGIN
+	ALTER DATABASE MyDatabase SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE MyDatabase
+END;
+GO
+
+-- Create Database [MyDatabase]
+CREATE DATABASE MyDatabase;
+GO
+
+-- Switch to newly created database
+USE MyDatabase;
+GO
+-- Create schema [landing]
+CREATE SCHEMA landing;
+GO
+
+-- Create schema [bronze]
+CREATE SCHEMA bronze;
+GO
+
+-- Create schema [silver]
+CREATE SCHEMA silver;
+GO
+
+-- Create schema [gold]
+CREATE SCHEMA gold;
+GO
+
+-- Create schema [audit]
+CREATE SCHEMA [audit];
+GO
