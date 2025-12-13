9400a6704f8236822b8bff700756bd2f69cc0804
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-13 10:30:34 +0000

    Update 01_ddl_script_etl_log_tables.sql

--- scripts/04_audit/01_ddl_script_etl_log_tables.sql
+++ scripts/04_audit/01_ddl_script_etl_log_tables.sql
@@ -44,8 +44,8 @@ CREATE TABLE [audit].etl_job_run
 	job_run_id UNIQUEIDENTIFIER NOT NULL,
 	pipeline_name NVARCHAR(50) NOT NULL,
 	start_time DATETIME NOT NULL,
-	end_time DATETIME NOT NULL,
-	job_run_duration_seconds INT NOT NULL,
+	end_time DATETIME,
+	job_run_duration_seconds INT,
 	job_run_status NVARCHAR(50) NOT NULL,
 	err_message NVARCHAR(MAX),
 	trigger_type NVARCHAR(50),
@@ -60,17 +60,18 @@ GO
 CREATE TABLE [audit].etl_step_run
 (
 	step_run_id UNIQUEIDENTIFIER NOT NULL,
-	job_run_id UNIQUEIDENTIFIER NOT NULL,
+	job_run_id UNIQUEIDENTIFIER,
 	layer NVARCHAR(50) NOT NULL,
 	table_name NVARCHAR(50) NOT NULL,
 	step_name NVARCHAR(50) NOT NULL,
 	start_time DATETIME NOT NULL,
-	end_time DATETIME NOT NULL,
-	step_run_duration_seconds INT NOT NULL,
-	step_run_status NVARCHAR(50) NOT NULL,
+	end_time DATETIME,
+	step_run_duration_seconds INT,
+	step_run_status NVARCHAR(50),
 	rows_source INT,
 	rows_loaded INT,
 	rows_diff INT,
+	file_path NVARCHAR(MAX),
 	err_message NVARCHAR(MAX),
 	CONSTRAINT pk_etl_step_run PRIMARY KEY(step_run_id),
 	CONSTRAINT fk_etl_step_run_etl_job_run FOREIGN KEY(job_run_id) REFERENCES [audit].etl_job_run(job_run_id)
@@ -83,11 +84,11 @@ GO
 
 CREATE TABLE [audit].etl_error_log
 (
-	error_run_id UNIQUEIDENTIFIER NOT NULL,
-	job_run_id UNIQUEIDENTIFIER NOT NULL,
+	error_run_id UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+	job_run_id UNIQUEIDENTIFIER,
 	step_run_id UNIQUEIDENTIFIER NOT NULL,
 	err_procedure NVARCHAR(50) NOT NULL,
-	err_timestamp DATETIME NOT NULL,
+	err_timestamp DATETIME DEFAULT GETDATE() NOT NULL,
 	err_message NVARCHAR(MAX) NOT NULL,
 	err_state INT NOT NULL,
 	err_line INT NOT NULL,
@@ -105,14 +106,14 @@ GO
 CREATE TABLE [audit].etl_data_quality
 (
 	dq_run_id UNIQUEIDENTIFIER NOT NULL,
-	job_run_id UNIQUEIDENTIFIER NOT NULL,
+	job_run_id UNIQUEIDENTIFIER,
 	step_run_id UNIQUEIDENTIFIER NOT NULL,
-	dq_timestamp DATETIME NOT NULL,
+	dq_timestamp DATETIME DEFAULT GETDATE() NOT NULL,
 	dq_check_name NVARCHAR(50) NOT NULL,
 	dq_status NVARCHAR(50) NOT NULL,
-	expected_rows INT NOT NULL,
-	actual_rows INT NOT NULL,
-	err_detail NVARCHAR(1000) NULL,
+	expected_rows INT,
+	actual_rows INT,
+	err_detail NVARCHAR(1000),
 	CONSTRAINT pk_etl_data_quality PRIMARY KEY(dq_run_id),
 	CONSTRAINT fk_etl_data_quality_etl_job_run FOREIGN KEY(job_run_id) REFERENCES [audit].etl_job_run(job_run_id),
 	CONSTRAINT fk_etl_data_quality_etl_step_run FOREIGN KEY(step_run_id) REFERENCES [audit].etl_step_run(step_run_id)
