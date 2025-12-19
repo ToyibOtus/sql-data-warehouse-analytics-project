5600668b09790621fda636557723dc77297e041f
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-19 15:54:17 +0000

    Create DDL script for silver layer tables
    This script creates and designs the structure of 6 silver tables, including crm_cust_info, crm_prd_info, crm_sales_details, erp_cust_az12, erp_loc_a101, and erp_px_cat_g1v2. It includes dropping existing tables if they exist and creating new ones with specified columns and data types.

---
+++ scripts/05_silver/01_ddl_scrip.sql
@@ -0,0 +1,125 @@
+/*
+=======================================================================
+DDL Script: Build Silver Layer
+=======================================================================
+Script Purpose:
+	This script creates and design the structure of 6 silver tables.
+	Run this script to change the structure of your silver tables.
+=======================================================================
+*/
+-- Drop Table silver.crm_cust_info
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_cust_info;
+GO
+
+-- Create Table silver.crm_cust_info
+CREATE TABLE silver.crm_cust_info
+(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_first_name NVARCHAR(50),
+	cst_last_name NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+-- Drop Table silver.crm_prd_info
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_prd_info;
+GO
+
+-- Create Table silver.crm_prd_info
+CREATE TABLE silver.crm_prd_info
+(
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+-- Drop Table silver.crm_sales_details
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+DROP TABLE silver.crm_sales_details;
+GO
+
+-- Create Table silver.crm_sales_details
+CREATE TABLE silver.crm_sales_details
+(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+-- Drop Table silver.erp_cust_az12
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+DROP TABLE silver.erp_cust_az12;
+GO
+
+-- Create Table silver.erp_cust_az12
+CREATE TABLE silver.erp_cust_az12
+(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+-- Drop Table silver.erp_loc_a101
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+DROP TABLE silver.erp_loc_a101;
+GO
+
+-- Create Table silver.erp_loc_a101
+CREATE TABLE silver.erp_loc_a101
+(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+-- Drop Table silver.erp_px_cat_g1v2
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+-- Create Table silver.erp_px_cat_g1v2
+CREATE TABLE silver.erp_px_cat_g1v2
+(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_step_run_id UNIQUEIDENTIFIER NOT NULL,
+	dwh_raw_row NVARCHAR(MAX),
+	dwh_row_hash VARBINARY(32),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
