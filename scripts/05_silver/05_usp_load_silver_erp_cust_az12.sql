5543ce0d9637d1f5e4f6a4993828f923410f3a45
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-25 12:47:19 +0000

    Refactor error handling and data quality checks

--- scripts/05_silver/05_usp_load_silver_erp_cust_az12.sql
+++ scripts/05_silver/05_usp_load_silver_erp_cust_az12.sql
@@ -98,11 +98,10 @@ BEGIN
 			cid,
 			bdate,
 			gen) AS dwh_raw_row,
-			HASHBYTES('SHA2_256',
-			CAST(CONCAT_WS('|',
-			cid,
-			bdate,
-			gen) AS VARBINARY(MAX))) AS dwh_row_hash
+			HASHBYTES('SHA2_256', CONCAT_WS('|',
+			COALESCE(CAST(cid AS VARBINARY(MAX)), '~'),
+			COALESCE(CAST(bdate AS VARBINARY(MAX)), '~'),
+			COALESCE(CAST(gen AS VARBINARY(MAX)), '~'))) AS dwh_row_hash
 		FROM data_transformations
 		)
 		-- Retrieve newly transformed records and load into corresponding silver staging table
@@ -135,7 +134,7 @@ BEGIN
 		BEGIN
 			SET @end_time = GETDATE();
 			SET @step_duration = DATEDIFF(second, @start_time, @end_time);
-			SET @step_status = 'FAILED';
+			SET @step_status = 'NO OPERATION';
 			SET @rows_source = 0;
 			SET @rows_loaded = 0;
 			SET @rows_diff = 0;
@@ -148,7 +147,7 @@ BEGIN
 					rows_source = @rows_source,
 					rows_loaded = @rows_loaded,
 					rows_diff = @rows_diff,
-					err_message = 'No new records from source table "' + @source_path + '".'
+					msg = 'No new records from source table "' + @source_path + '".'
 				WHERE step_run_id = @step_run_id;
 
 			RETURN;
@@ -177,45 +176,26 @@ BEGIN
 			dq_check_name,
 			rows_checked,
 			rows_failed,
-			dq_status
+			dq_status,
+			err_detail
 		)
 		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'pk_nulls', rows_checked, pk_nulls AS rows_failed,
-		CASE WHEN pk_nulls > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status FROM #dq_metrics_erp_cust_az12
+		CASE WHEN pk_nulls > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status, CASE WHEN pk_nulls > 0 THEN
+		'CRITICAL RULE "pk_nulls" VIOLATED: Unable to load "' + @table_name + '".' ELSE NULL END AS err_detail
+		FROM #dq_metrics_erp_cust_az12
 		UNION ALL
 		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'pk_duplicates', rows_checked, pk_duplicates AS rows_failed,
-		CASE WHEN pk_duplicates > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status FROM #dq_metrics_erp_cust_az12
-		UNION ALL
-		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'invalid_bdate', rows_checked, invalid_bdate AS rows_failed,
-		CASE WHEN invalid_bdate > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status FROM #dq_metrics_erp_cust_az12
-		UNION ALL
-		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'invalid_gen', rows_checked, invalid_gen AS rows_failed,
-		CASE WHEN invalid_gen > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status FROM #dq_metrics_erp_cust_az12;
-
-		-- Map values to variables
-		SELECT @pk_nulls = rows_failed FROM [audit].etl_data_quality
-		WHERE step_run_id = @step_run_id AND dq_check_name = 'pk_nulls';
-
-		SELECT @pk_duplicates = rows_failed FROM [audit].etl_data_quality
-		WHERE step_run_id = @step_run_id AND dq_check_name = 'pk_duplicates';
-
-		-- Update table etl_data_quality when PK contain NULLs
-		IF @pk_nulls > 0
-		BEGIN
-			UPDATE [audit].etl_data_quality
-				SET err_detail = 'Critical DQ Failure: pk_nulls'
-			WHERE step_run_id = @step_run_id AND dq_check_name = 'pk_nulls';
-		END;
-
-		-- Update table etl_data_quality when PK exist as duplicates
-		IF @pk_duplicates > 0
-		BEGIN
-			UPDATE [audit].etl_data_quality
-				SET err_detail = 'Critical DQ Failure: pk_duplicates'
-			WHERE step_run_id = @step_run_id AND dq_check_name = 'pk_duplicates';
-		END;
+		CASE WHEN pk_duplicates > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status, CASE WHEN pk_duplicates > 0 THEN
+		'CRITICAL RULE "pk_duplicates" VIOLATED: Unable to load "' + @table_name + '".' ELSE NULL END AS err_detail
+		FROM #dq_metrics_erp_cust_az12;
 
 		-- Controlled error handling when critical data quality rule is violated
-		IF @pk_nulls > 0 OR @pk_duplicates > 0
+		IF EXISTS
+		(
+			SELECT 1 FROM [audit].etl_data_quality dq INNER JOIN [audit].etl_data_quality_control dqc
+			ON dq.dq_layer = dqc.dq_layer AND dq.dq_table_name = dqc.dq_table_name AND dq.dq_check_name = dqc.dq_check_name
+			WHERE dq.step_run_id = @step_run_id AND dq.dq_status = 'FAILED' AND dqc.stop_on_failure = 1 AND dqc.is_active = 1
+		)
 		BEGIN
 			SET @end_time = GETDATE();
 			SET @step_duration = DATEDIFF(second, @start_time, @end_time);
@@ -232,7 +212,7 @@ BEGIN
 					rows_source = @rows_source,
 					rows_loaded = @rows_loaded,
 					rows_diff = @rows_diff,
-					err_message = 'Critical data quality rule violated. Check log table "etl_data_quality" to know which rule was violated.'
+					msg = 'Critical data quality rule violated. Check log table "etl_data_quality" to know which rule was violated.'
 				WHERE step_run_id = @step_run_id;
 
 			RETURN;
@@ -327,7 +307,7 @@ BEGIN
 			rows_source = @rows_source,
 			rows_loaded = @rows_loaded,
 			rows_diff = @rows_diff,
-			err_message = ERROR_MESSAGE()
+			msg = ERROR_MESSAGE()
 		WHERE step_run_id = @step_run_id;
 
 		-- Load error details into log table
