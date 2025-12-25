0002288772d6448fcd86c52f68bc9634af98d4b1
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-25 14:22:23 +0000

    Update 06_usp_load_landing_erp_loc_a101.sql

--- scripts/02_landing/06_usp_load_landing_erp_loc_a101.sql
+++ scripts/02_landing/06_usp_load_landing_erp_loc_a101.sql
@@ -1,44 +1,47 @@
 /*
-=====================================================================================================
-User Stored Procedure: Load Landing Table (CSV file -> Landing)
-=====================================================================================================
+==================================================================================================================
+User Stored Procedure: Load Silver Table (Bronze -> Silver)
+==================================================================================================================
 Script Purpose:
-	This script loads [landing.erp_loc_a101]. It also performs logging operations by loading vital
-	log details into log tables [etl_step_run] & [etl_error_log].
+	This script loads the silver table [silver.erp_loc_a101]. It performs series of data transformations such
+	as data cleansing, enrichment, and standardization.
 
 Parameter: @job_run_id UNIQUEIDENTIFIER = NULL
 
-Usage: EXEC landing.usp_load_landing_erp_loc_a101;
+Usage: EXEC silver.usp_load_silver_erp_loc_a101;
 
 Note:
-	The default value of job_run_id is NULL if it is run independently.
-	Ensure to run the master stored procedure as it performs a full ETL run and assigns same 
-	job_run_id across all layers and tables within the same run.
-=====================================================================================================
+	* Running this script independently will assign NULL to the parameter @job_run_id is none is provided.
+	* Ensure to run the master procedure as it performs a full ETL run and assigns similar job_run_id 
+	  across tables and layers within the same ETL run, allowing for unified tracking.
+==================================================================================================================
 */
-CREATE OR ALTER PROCEDURE landing.usp_load_landing_erp_loc_a101 @job_run_id UNIQUEIDENTIFIER = NULL AS
+CREATE OR ALTER PROCEDURE silver.usp_load_silver_erp_loc_a101 @job_run_id UNIQUEIDENTIFIER = NULL AS 
 BEGIN
 	-- Abort transaction on severe error
 	SET XACT_ABORT ON;
 
-	-- Declare and assign values to variables
+	-- Declare and map values to variables where necessary
 	DECLARE
 	@step_run_id UNIQUEIDENTIFIER = NEWID(),
-	@layer NVARCHAR(50) = 'landing',
+	@layer NVARCHAR(50) = 'silver',
 	@table_name NVARCHAR(50) = 'erp_loc_a101',
-	@step_name NVARCHAR(50) = 'usp_load_landing_erp_loc_a101',
+	@step_name NVARCHAR(50) = 'usp_load_silver_erp_loc_a101',
 	@start_time DATETIME,
 	@end_time DATETIME,
 	@step_duration INT,
 	@step_status NVARCHAR(50) = 'RUNNING',
+	@rows_source INT,
 	@rows_loaded INT,
-	@source_path NVARCHAR(MAX) = 'C:\Users\PC\Documents\SQL_DataWareHouseProject\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv',
-	@sql NVARCHAR(MAX);
+	@rows_diff INT,
+	@source_path NVARCHAR(50) = 'bronze.erp_loc_a101',
+	@pk_nulls INT,
+	@pk_duplicates INT;
 
-	-- Capture start time
+	-- Capture start_time
 	SET @start_time = GETDATE();
-	
-	-- Insert into log table
+
+	-- Load log table
 	INSERT INTO [audit].etl_step_run
 	(
 		step_run_id,
@@ -63,15 +66,184 @@ BEGIN
 	);
 
 	BEGIN TRY
-		-- Begin Transaction
-		BEGIN TRAN;
-		-- Truncate table before loading
-		TRUNCATE TABLE landing.erp_loc_a101;
+		-- Delete data in silver staging table
+		TRUNCATE TABLE silver_stg.erp_loc_a101;
 
-		-- Map string value to variable
-		SET @sql = 'BULK INSERT landing.erp_loc_a101 FROM ''' + @source_path + ''' WITH (FIRST_ROW = 2, FIELDTERMINATOR = '','', TABLOCK);';
-		-- Load data to table
-		EXEC (@sql);
+		-- Perform data transformations on source table
+		WITH data_transformations AS
+		(
+		SELECT
+			TRIM(REPLACE(cid, '-', '')) AS cid,
+			CASE 
+				WHEN UPPER(TRIM(cntry)) IN ('DE', 'Germany') THEN 'Germany'
+				WHEN UPPER(TRIM(cntry)) IN ('USA', 'US', 'United States') THEN 'United States'
+				WHEN cntry IS NULL OR TRIM(cntry) = '' THEN 'Unknown'
+				ELSE TRIM(cntry)
+			END AS cntry
+		FROM bronze.erp_loc_a101
+		)
+		, metadata_columns AS
+		(
+		SELECT
+			cid,
+			cntry,
+			@step_run_id AS dwh_step_run_id,
+			CONCAT_WS('|',
+			cid,
+			cntry) AS dwh_raw_row,
+			HASHBYTES('SHA2_256', CONCAT_WS('|',
+			COALESCE(CAST(cid AS VARBINARY(MAX)), '~'),
+			COALESCE(CAST(cntry AS VARBINARY(MAX)), '~'))) AS dwh_row_hash
+		FROM data_transformations
+		)
+		-- Retrieve newly transformed records and load into corresponding silver staging table
+		INSERT INTO silver_stg.erp_loc_a101
+		(
+			cid,
+			cntry,
+			dwh_step_run_id,
+			dwh_raw_row,
+			dwh_row_hash
+		)
+		SELECT
+			mc.cid,
+			mc.cntry,
+			mc.dwh_step_run_id,
+			mc.dwh_raw_row,
+			mc.dwh_row_hash
+		FROM metadata_columns mc
+		LEFT JOIN silver.erp_loc_a101 sel
+		ON mc.cid = sel.cid 
+		AND mc.dwh_row_hash = sel.dwh_row_hash
+		WHERE sel.dwh_row_hash IS NULL;
+
+		-- Retrieve total number of new records from silver_stg
+		SELECT @rows_source = COUNT(*) FROM silver_stg.erp_loc_a101;
+
+		-- Error handling if total number of new records is NULL or zero
+		IF @rows_source IS NULL OR @rows_source = 0
+		BEGIN
+			SET @end_time = GETDATE();
+			SET @step_duration = DATEDIFF(second, @start_time, @end_time);
+			SET @step_status = 'NO OPERATION';
+			SET @rows_source = 0;
+			SET @rows_loaded = 0;
+			SET @rows_diff = 0;
+
+			UPDATE [audit].etl_step_run
+				SET 
+					end_time = @end_time,
+					step_run_duration_seconds = @step_duration,
+					step_run_status = @step_status,
+					rows_source = @rows_source,
+					rows_loaded = @rows_loaded,
+					rows_diff = @rows_diff,
+					msg = 'No new records from source table "' + @source_path + '".'
+				WHERE step_run_id = @step_run_id;
+
+			RETURN;
+		END;
+
+		-- Drop temporary table if it exist
+		DROP TABLE IF EXISTS #dq_metrics_erp_loc_a101;
+
+		-- Create and load into temp table vital dq metrics
+		SELECT
+			COUNT(*) AS rows_checked,
+			SUM(CASE WHEN cid IS NULL THEN 1 ELSE 0 END) AS pk_nulls,
+			COUNT(cid) - COUNT(DISTINCT cid) AS pk_duplicates
+			INTO #dq_metrics_erp_loc_a101
+		FROM silver_stg.erp_loc_a101;
+
+		-- Insert into log etl_data_quality
+		INSERT INTO [audit].etl_data_quality
+		(
+			job_run_id,
+			step_run_id,
+			dq_layer,
+			dq_table_name,
+			dq_check_name,
+			rows_checked,
+			rows_failed,
+			dq_status,
+			err_detail
+		)
+		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'pk_nulls', rows_checked, pk_nulls AS rows_failed,
+		CASE WHEN pk_nulls > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status, CASE WHEN pk_nulls > 0 THEN
+		'CRITICAL RULE "pk_nulls" VIOLATED: Unable to load "' + @table_name + '".' ELSE NULL END AS err_detail
+		FROM #dq_metrics_erp_loc_a101
+		UNION ALL
+		SELECT @job_run_id, @step_run_id, @layer, @table_name, 'pk_duplicates', rows_checked, pk_duplicates AS rows_failed,
+		CASE WHEN pk_duplicates > 0 THEN 'FAILED' ELSE 'SUCCESS' END AS dq_status, CASE WHEN pk_duplicates > 0 THEN
+		'CRITICAL RULE "pk_duplicates" VIOLATED: Unable to load "' + @table_name + '".' ELSE NULL END AS err_detail 
+		FROM #dq_metrics_erp_loc_a101;
+
+		-- Controlled error handling when critical data quality rule is violated
+		IF EXISTS
+		(
+			SELECT 1 FROM [audit].etl_data_quality dq INNER JOIN [audit].etl_data_quality_control dqc
+			ON dq.dq_layer = dqc.dq_layer AND dq.dq_table_name = dqc.dq_table_name AND dq.dq_check_name = dqc.dq_check_name
+			WHERE dq.step_run_id = @step_run_id AND dq.dq_status = 'FAILED' AND dqc.stop_on_failure = 1 AND dqc.is_active = 1
+		)
+		BEGIN
+			SET @end_time = GETDATE();
+			SET @step_duration = DATEDIFF(second, @start_time, @end_time);
+			SET @step_status = 'FAILED';
+			IF @rows_source IS NULL SET @rows_source = 0;
+			SET @rows_loaded = 0;
+			SET @rows_diff = @rows_source - @rows_loaded;
+
+			UPDATE [audit].etl_step_run
+				SET
+					end_time = @end_time,
+					step_run_duration_seconds = @step_duration,
+					step_run_status = @step_status,
+					rows_source = @rows_source,
+					rows_loaded = @rows_loaded,
+					rows_diff = @rows_diff,
+					msg = 'Critical data quality rule violated. Check log table "etl_data_quality" to know which rule was violated.'
+				WHERE step_run_id = @step_run_id;
+
+			RETURN;
+		END;
+
+		-- Begin transaction
+		BEGIN TRAN;
+
+		-- Merge into target using source
+		MERGE silver.erp_loc_a101 tgt
+		USING silver_stg.erp_loc_a101 src
+			ON tgt.cid = src.cid
+
+		-- Update outdated records
+		WHEN MATCHED 
+			AND tgt.dwh_row_hash != src.dwh_row_hash THEN
+		UPDATE SET
+			tgt.cntry = src.cntry,
+			tgt.dwh_step_run_id = src.dwh_step_run_id,
+			tgt.dwh_raw_row = src.dwh_raw_row,
+			tgt.dwh_row_hash = src.dwh_row_hash
+
+		-- Insert new records
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT
+			(
+				cid,
+				cntry,
+				dwh_step_run_id,
+				dwh_raw_row,
+				dwh_row_hash
+			)
+			VALUES
+			(
+				src.cid,
+				src.cntry,
+				src.dwh_step_run_id,
+				src.dwh_raw_row,
+				src.dwh_row_hash
+			);
+		-- Retrieve total rows loaded
+		SET @rows_loaded = @@ROWCOUNT;
 
 		-- Finalize transaction on success
 		COMMIT TRAN;
@@ -80,15 +252,17 @@ BEGIN
 		SET @end_time = GETDATE();
 		SET @step_duration = DATEDIFF(second, @start_time, @end_time);
 		SET @step_status = 'SUCCESS';
-		SELECT @rows_loaded = COUNT(*) FROM landing.erp_loc_a101;
+		SET @rows_diff = @rows_source - @rows_loaded;
 
-		-- Update log table on success
+		-- Update log table etl_step_run on success
 		UPDATE [audit].etl_step_run
-		SET
+			SET
 			end_time = @end_time,
 			step_run_duration_seconds = @step_duration,
 			step_run_status = @step_status,
-			rows_loaded = @rows_loaded
+			rows_source = @rows_source,
+			rows_loaded = @rows_loaded,
+			rows_diff = @rows_diff
 		WHERE step_run_id = @step_run_id;
 	END TRY
 
@@ -98,42 +272,50 @@ BEGIN
 		SET @step_duration = DATEDIFF(second, @start_time, @end_time);
 		SET @step_status = 'FAILED';
 
-		-- Rollback transaction on error
+		-- Map zero to row source if NULL
+		IF @rows_source IS NULL SET @rows_source = 0;
+
+		-- Rollback transaction on errror
 		IF @@TRANCOUNT > 0 ROLLBACK TRAN;
 
-		-- Set rows loaded to 0 when NULL
+		-- Map zero to rows loaded if NULL
 		IF @rows_loaded IS NULL SET @rows_loaded = 0;
 
-		-- Update log table on failure
+		-- Calculate row difference
+		SET @rows_diff = @rows_source - @rows_loaded;
+
+		-- Update etl_step_run on failure
 		UPDATE [audit].etl_step_run
 		SET
 			end_time = @end_time,
 			step_run_duration_seconds = @step_duration,
 			step_run_status = @step_status,
+			rows_source = @rows_source,
 			rows_loaded = @rows_loaded,
-			err_message = ERROR_MESSAGE()
+			rows_diff = @rows_diff,
+			msg = ERROR_MESSAGE()
 		WHERE step_run_id = @step_run_id;
-	
-		-- Insert error_details into log table
+
+		-- Load error details into log table
 		INSERT INTO [audit].etl_error_log
 		(
-			job_run_id, 
-			step_run_id, 
+			job_run_id,
+			step_run_id,
 			err_procedure,
 			err_number,
-			err_message, 
-			err_state, 
+			err_message,
+			err_state,
 			err_line,
 			err_severity
 		)
 		VALUES
 		(
-			@job_run_id, 
-			@step_run_id, 
+			@job_run_id,
+			@step_run_id,
 			@step_name,
 			ERROR_NUMBER(),
-			ERROR_MESSAGE(), 
-			ERROR_STATE(), 
+			ERROR_MESSAGE(),
+			ERROR_STATE(),
 			ERROR_LINE(),
 			ERROR_SEVERITY()
 		);
