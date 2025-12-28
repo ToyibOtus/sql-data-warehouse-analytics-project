52426b75002636e1b9656b94b388f72dcb8ffd3b
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-28 16:21:35 +0000

    Update 01_change_over_time_analysis.sql

--- scripts/10_advanced_data_analytics/01_change_over_time_analysis.sql
+++ scripts/10_advanced_data_analytics/01_change_over_time_analysis.sql
@@ -18,7 +18,7 @@ SELECT
 	SUM(sales) AS total_sales
 FROM gold.vw_fact_sales
 WHERE YEAR(order_date) IS NOT NULL
-GROUP BY YEAR(order_date);
+GROUP BY order_date;
 
 -- Month-Over-Month Analysis
 SELECT
@@ -30,6 +30,6 @@ SELECT
 	SUM(quantity) AS total_quantity,
 	SUM(sales) AS total_sales
 FROM gold.vw_fact_sales
-WHERE YEAR(order_date) IS NOT NULL
+WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(month, order_date)
 ORDER BY DATETRUNC(month, order_date);
