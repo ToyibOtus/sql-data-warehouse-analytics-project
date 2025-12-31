efaa2e1eeaa720cf153e71f4f98fda110d0d8055
Toyib Otusanya <toyibotusanya@gmail.com>
2025-12-31 12:57:13 +0000

    Update 06_usp_load_silver_erp_loc_a101.sql

--- scripts/05_silver/06_usp_load_silver_erp_loc_a101.sql
+++ scripts/05_silver/06_usp_load_silver_erp_loc_a101.sql
@@ -11,7 +11,7 @@ Parameter: @job_run_id UNIQUEIDENTIFIER = NULL
 Usage: EXEC silver.usp_load_silver_erp_loc_a101;
 
 Note:
-	* Running this script independently will assign NULL to the parameter @job_run_id is none is provided.
+	* Running this script independently assigns a job_run_id local to this procedure.
 	* Ensure to run the master procedure as it performs a full ETL run and assigns similar job_run_id 
 	  across tables and layers within the same ETL run, allowing for unified tracking.
 ==================================================================================================================
@@ -37,6 +37,9 @@ BEGIN
 	@source_path NVARCHAR(50) = 'bronze.erp_loc_a101',
 	@pk_nulls INT,
 	@pk_duplicates INT;
+
+	-- Map value to job_run_id if NULL
+	IF @job_run_id IS NULL SET @job_run_id = NEWID();
 
 	-- Capture start_time
 	SET @start_time = GETDATE();
@@ -87,7 +90,7 @@ BEGIN
 		SELECT
 			cid,
 			cntry,
-			@step_run_id AS dwh_step_run_id,
+			@job_run_id AS dwh_job_run_id,
 			CONCAT_WS('|',
 			cid,
 			cntry) AS dwh_raw_row,
@@ -101,14 +104,14 @@ BEGIN
 		(
 			cid,
 			cntry,
-			dwh_step_run_id,
+			dwh_job_run_id,
 			dwh_raw_row,
 			dwh_row_hash
 		)
 		SELECT
 			mc.cid,
 			mc.cntry,
-			mc.dwh_step_run_id,
+			mc.dwh_job_run_id,
 			mc.dwh_raw_row,
 			mc.dwh_row_hash
 		FROM metadata_columns mc
@@ -220,7 +223,7 @@ BEGIN
 			AND tgt.dwh_row_hash != src.dwh_row_hash THEN
 		UPDATE SET
 			tgt.cntry = src.cntry,
-			tgt.dwh_step_run_id = src.dwh_step_run_id,
+			tgt.dwh_job_run_id = src.dwh_job_run_id,
 			tgt.dwh_raw_row = src.dwh_raw_row,
 			tgt.dwh_row_hash = src.dwh_row_hash
 
@@ -230,7 +233,7 @@ BEGIN
 			(
 				cid,
 				cntry,
-				dwh_step_run_id,
+				dwh_job_run_id,
 				dwh_raw_row,
 				dwh_row_hash
 			)
@@ -238,7 +241,7 @@ BEGIN
 			(
 				src.cid,
 				src.cntry,
-				src.dwh_step_run_id,
+				src.dwh_job_run_id,
 				src.dwh_raw_row,
 				src.dwh_row_hash
 			);
